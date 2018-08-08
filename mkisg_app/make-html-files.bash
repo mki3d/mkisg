9a8a692b452dd87670ebacc3750f6eab0904fa3e
Marcin Kik <mki1967@gmail.com>
2018-08-08 16:22:55 +0200

    moving the (stub) chrome app to mkisg_chrome subdirectory

--- mkisg_app/make-html-files.bash
+++ mkisg_app/make-html-files.bash
@@ -5,10 +5,10 @@ cat \
     html/body-html-suffix.html \
     > mkisg.html
 
-# make Chrome App
-cat \
-    html/html-prefix.html \
-    html/index-head.html \
-    html/body-html-suffix.html \
-    > index.html
+# make Chrome App -- removed - the app is now developed as PWA only
+# cat \
+#    html/html-prefix.html \
+#    html/index-head.html \
+#    html/body-html-suffix.html \
+#    > index.html
 
